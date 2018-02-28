0941cf46ed4f7d9da38d086877d2d9a78f92380a
softcomet <professinalcertifiedsenior.dev@gmail.com>
2018-02-28 07:47:14 +0100

    Scrapiing using Selenium

--- scraper/spiders/scrapingdata.py
+++ scraper/spiders/scrapingdata.py
@@ -6,6 +6,7 @@ import re
 import json
 import os
 import time
+import time
 # from pyvirtualdisplay import Display
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
@@ -32,16 +33,60 @@ class SiteProductItem(scrapy.Item):
 class NewEvents (scrapy.Spider):
 
     name = "scrapingdata"
-    allowed_domains = ['www.oocl.com']
-    start_urls = ['http://www.oocl.com/eng/Pages/default.aspx']
+    allowed_domains = ['www.shipmentlink.com']
+    start_urls = ['https://www.shipmentlink.com/servlet/TDB1_CargoTracking.do']
 
     def start_requests(self):
         start_urls = self.start_urls
-        # for start_url in start_urls:
-        #     yield scrapy.Request(url=start_url, callback=self.parse_pages)
         yield scrapy.Request(url=start_urls[0], callback=self.parse_pages)
+    #
+    # def parse_pages(self, response):
+    #     page_links = []
+    #
+    #     if response.url == 'https://www.vapordna.com/Devices-s/1814.htm':
+    #         page_links = ['https://www.vapordna.com/Devices-s/1814.htm']
+    #
+    #     for page_link in page_links:
+    #         yield scrapy.Request(url=page_link, callback=self._parse_product_links, dont_filter=True)
+    #
+    # def _parse_product_links(self, response):
+    #
+    #     if response.xpath('//div[@class="product__image"]/a/@href').extract():
+    #         prods = response.xpath('//div[@class="product__image"]/a/@href').extract()
+    #         for prod in prods:
+    #             yield Request(url='https://www.electrictobacconist.com' + prod, callback=self.parse_product)
+    #
+    # def parse_product(self, response):
+    #     prod_item = SiteProductItem()
+    #     name = self._parse_name(response)
+    #     price = self._parse_price(response)
+    #     url = self._parse_url(response)
+    #     prod_item['Name'] = name
+    #     prod_item['Price'] = price
+    #     prod_item['Product_Url'] = url
+    #     return prod_item
+    #
+    # @staticmethod
+    # def _parse_name(response):
+    #     if response.xpath('//span[@itemprop="name"]//text()').extract():
+    #         name = response.xpath('//span[@itemprop="name"]//text()').extract()[0]
+    #
+    #     return str(name) if name else " "
+    #
+    # @staticmethod
+    # def _parse_price(response):
+    #     if response.xpath('//span[@itemprop="price"]//text()').extract():
+    #         price = response.xpath('//span[@itemprop="price"]//text()').extract()
+    #
+    #     return str(price[0]) if price else " "
+    #
+    # @staticmethod
+    # def _parse_url(response):
+    #     url = response.url
+    #     return url
 
     def parse_pages(self, response):
+
         options = webdriver.ChromeOptions()
         # options.add_argument('headless')
         browser = webdriver.Chrome(
@@ -54,18 +99,17 @@ class NewEvents (scrapy.Spider):
         browser.maximize_window()
         browser.implicitly_wait(100)
         browser.set_page_load_timeout(100)
+        # time.sleep(3)
         browser.get(response.url)
-        # if browser.find_element_by_xpath('//ul[contains(@class, "dropdown-menu")]'):
-        #     search_type = browser.find_element_by_xpath('//ul[contains(@class, "dropdown-menu")]'
-        #                                                 '/li[3]/a')
-        # if not search_type:
-        search_type = browser.find_element_by_xpath('//select[@id="ooclCargoSelector"]/option[@value="cont"]')
-        if search_type:
-            search_type.click()
 
-        search_input = browser.find_element_by_id("SEARCH_NUMBER")
-        search_input.send_keys('LEAU4908966')
-        search_button = browser.find_element_by_id('container_btn')
+        # if browser.find_element_by_xpath('//select[@name="TYPE"]/option[@value="CNTR"]'):
+        #     search_type = browser.find_element_by_xpath('//select[@name="TYPE"]/option[@value="CNTR"]')
+        #     search_type.click()
+        if browser.find_element_by_xpath('//input[@name="CargoNO"]'):
+            search_input = browser.find_element_by_xpath('//input[@name="CargoNO"]')
+            search_input.click()
+            search_input.send_keys('LEAU4908755')
+        search_button = browser.find_element_by_xpath('//a[@href="javascript:submit(13)"]')
         search_button.click()
 
         page_content = browser.page_source
@@ -74,4 +118,8 @@ class NewEvents (scrapy.Spider):
         product['Origin'] = page_content
 
         return product
+
+
+
+
 

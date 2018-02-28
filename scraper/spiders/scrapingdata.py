8d9a26aa0c5511f8cfd81ee24e99c8096b36568d
softcomet <professinalcertifiedsenior.dev@gmail.com>
2018-02-28 10:26:50 +0100

    scraping data from https://www.shipmentlink.com/ using Scrapy

--- scraper/spiders/scrapingdata.py
+++ scraper/spiders/scrapingdata.py
@@ -11,23 +11,19 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
-
-CWD = os.path.dirname(os.path.abspath(__file__))
-driver_path = os.path.join(CWD, 'bin', 'chromedriver')
-driver_log_path = os.path.join(CWD, 'bin', 'driver.log')
+# CWD = os.path.dirname(os.path.abspath(__file__))
+# driver_path = os.path.join(CWD, 'bin', 'chromedriver')
+# driver_log_path = os.path.join(CWD, 'bin', 'driver.log')
 
 
 class SiteProductItem(scrapy.Item):
-    # ContainerNumber = scrapy.Field()
-    # ContainerSizeType = scrapy.Field()
-    # ShippingLine = scrapy.Field()
-    # LatestEvent = scrapy.Field()
-    Origin = scrapy.Field()
-    # FinalDestination = scrapy.Field()
-    # ETA = scrapy.Field()
-    # PortofDischarge = scrapy.Field()
-    # Destination = scrapy.Field()
-    # EmptyReturnLocation = scrapy.Field()
+    ContainerNumber = scrapy.Field()
+    ContainerSizeType = scrapy.Field()
+    Date = scrapy.Field()
+    ContainerMoves = scrapy.Field()
+    Location = scrapy.Field()
+    VesselVoyage = scrapy.Field()
+    VGM = scrapy.Field()
 
 
 class NewEvents (scrapy.Spider):
@@ -35,55 +31,72 @@ class NewEvents (scrapy.Spider):
     name = "scrapingdata"
     allowed_domains = ['www.shipmentlink.com']
     start_urls = ['https://www.shipmentlink.com/servlet/TDB1_CargoTracking.do']
+    form_data = {'CNTR': 'LEAU4908755', 'TYPE': 'CNTR'}
+    headers = {'Referer': 'https://www.shipmentlink.com/servlet/TDB1_CargoTracking.do',
+               'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                             'Chrome/64.0.3282.186 Safari/537.36',
+               'X-Requested-With': 'XMLHttpRequest',
+               'Content-Type': 'application/x-www-form-urlencoded'
+               }
 
     def start_requests(self):
         start_urls = self.start_urls
-        yield scrapy.Request(url=start_urls[0], callback=self.parse_pages)
-
-    def parse_pages(self, response):
-        page_links = []
-
-        if response.url == 'https://www.vapordna.com/Devices-s/1814.htm':
-            page_links = ['https://www.vapordna.com/Devices-s/1814.htm']
-
-        for page_link in page_links:
-            yield scrapy.Request(url=page_link, callback=self._parse_product_links, dont_filter=True)
-
-    def _parse_product_links(self, response):
-
-        if response.xpath('//div[@class="product__image"]/a/@href').extract():
-            prods = response.xpath('//div[@class="product__image"]/a/@href').extract()
-            for prod in prods:
-                yield Request(url='https://www.electrictobacconist.com' + prod, callback=self.parse_product)
+        yield Request(url=start_urls[0],
+                      callback=self.parse_product,
+                      headers=self.headers,
+                      method='POST',
+                      body=urlencode(self.form_data),
+                      dont_filter=True
+                      )
 
     def parse_product(self, response):
         prod_item = SiteProductItem()
-        name = self._parse_name(response)
-        price = self._parse_price(response)
-        url = self._parse_url(response)
-        prod_item['Name'] = name
-        prod_item['Price'] = price
-        prod_item['Product_Url'] = url
+        prod_item['ContainerNumber'] = self._parse_ContainerNumber(response)
+        prod_item['ContainerSizeType'] = self._parse_ContainerSizeType(response)
+        prod_item['Date'] = self._parse_Date(response)
+        prod_item['ContainerMoves'] = self._parse_ContainerMoves(response)
+        prod_item['Location'] = self._parse_Location(response)
+        prod_item['VesselVoyage'] = self._parse_VesselVoyage(response)
+        prod_item['VGM'] = self._parse_VGM(response)
+
         return prod_item
 
     @staticmethod
-    def _parse_name(response):
-        if response.xpath('//span[@itemprop="name"]//text()').extract():
-            name = response.xpath('//span[@itemprop="name"]//text()').extract()[0]
-
-        return str(name) if name else " "
+    def _parse_ContainerNumber(response):
+        ContainerNumber = response.xpath('//table[@width="95%"][3]/tr[3]/td[1]//text()').extract()
+        return str(ContainerNumber[0]) if ContainerNumber else ' '
 
     @staticmethod
-    def _parse_price(response):
-        if response.xpath('//span[@itemprop="price"]//text()').extract():
-            price = response.xpath('//span[@itemprop="price"]//text()').extract()
-
-        return str(price[0]) if price else " "
+    def _parse_ContainerSizeType(response):
+        ContainerSizeType = response.xpath('//table[@width="95%"][3]/tr[3]/td[2]//text()').extract()
+        return str(ContainerSizeType[0]).strip() if ContainerSizeType else ' '
 
     @staticmethod
-    def _parse_url(response):
-        url = response.url
-        return url
+    def _parse_Date(response):
+        Date = response.xpath('//table[@width="95%"][3]/tr[3]/td[3]//text()').extract()
+        return str(Date[0]).strip() if Date else ' '
+
+    @staticmethod
+    def _parse_ContainerMoves(response):
+        ContainerMoves = response.xpath('//table[@width="95%"][3]/tr[3]/td[4]//text()').extract()
+        return str(ContainerMoves[0]).strip() if ContainerMoves else ' '
+
+    @staticmethod
+    def _parse_Location(response):
+        Location = response.xpath('//table[@width="95%"][3]/tr[3]/td[5]//text()').extract()
+        return str(Location[0]).strip() if Location else ' '
+
+    @staticmethod
+    def _parse_VesselVoyage(response):
+        VesselVoyage = response.xpath('//table[@width="95%"][3]/tr[3]/td[6]//text()').extract()
+        return str(VesselVoyage[0]).strip() if VesselVoyage else ' '
+
+    @staticmethod
+    def _parse_VGM(response):
+        VGM = response.xpath('//table[@width="95%"][3]/tr[3]/td[8]//text()').extract()
+        return str(VGM[0]).strip() if VGM else ' '
+
+
 
 
 

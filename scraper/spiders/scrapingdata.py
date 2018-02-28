17d959ce15af4f5195bdcd4e0a12104eb3a7e3a2
softcomet <professinalcertifiedsenior.dev@gmail.com>
2018-02-28 07:49:59 +0100

    scraping data using Python Scrapy

--- scraper/spiders/scrapingdata.py
+++ scraper/spiders/scrapingdata.py
@@ -39,85 +39,52 @@ class NewEvents (scrapy.Spider):
     def start_requests(self):
         start_urls = self.start_urls
         yield scrapy.Request(url=start_urls[0], callback=self.parse_pages)
-    #
-    # def parse_pages(self, response):
-    #     page_links = []
-    #
-    #     if response.url == 'https://www.vapordna.com/Devices-s/1814.htm':
-    #         page_links = ['https://www.vapordna.com/Devices-s/1814.htm']
-    #
-    #     for page_link in page_links:
-    #         yield scrapy.Request(url=page_link, callback=self._parse_product_links, dont_filter=True)
-    #
-    # def _parse_product_links(self, response):
-    #
-    #     if response.xpath('//div[@class="product__image"]/a/@href').extract():
-    #         prods = response.xpath('//div[@class="product__image"]/a/@href').extract()
-    #         for prod in prods:
-    #             yield Request(url='https://www.electrictobacconist.com' + prod, callback=self.parse_product)
-    #
-    # def parse_product(self, response):
-    #     prod_item = SiteProductItem()
-    #     name = self._parse_name(response)
-    #     price = self._parse_price(response)
-    #     url = self._parse_url(response)
-    #     prod_item['Name'] = name
-    #     prod_item['Price'] = price
-    #     prod_item['Product_Url'] = url
-    #     return prod_item
-    #
-    # @staticmethod
-    # def _parse_name(response):
-    #     if response.xpath('//span[@itemprop="name"]//text()').extract():
-    #         name = response.xpath('//span[@itemprop="name"]//text()').extract()[0]
-    #
-    #     return str(name) if name else " "
-    #
-    # @staticmethod
-    # def _parse_price(response):
-    #     if response.xpath('//span[@itemprop="price"]//text()').extract():
-    #         price = response.xpath('//span[@itemprop="price"]//text()').extract()
-    #
-    #     return str(price[0]) if price else " "
-    #
-    # @staticmethod
-    # def _parse_url(response):
-    #     url = response.url
-    #     return url
 
     def parse_pages(self, response):
+        page_links = []
 
-        options = webdriver.ChromeOptions()
-        # options.add_argument('headless')
-        browser = webdriver.Chrome(
-            executable_path=driver_path,
-            service_log_path=driver_log_path,
-            chrome_options=options
-        )
+        if response.url == 'https://www.vapordna.com/Devices-s/1814.htm':
+            page_links = ['https://www.vapordna.com/Devices-s/1814.htm']
 
-        # browser.set_window_size(1440, 900)
-        browser.maximize_window()
-        browser.implicitly_wait(100)
-        browser.set_page_load_timeout(100)
-        # time.sleep(3)
-        browser.get(response.url)
+        for page_link in page_links:
+            yield scrapy.Request(url=page_link, callback=self._parse_product_links, dont_filter=True)
 
-        # if browser.find_element_by_xpath('//select[@name="TYPE"]/option[@value="CNTR"]'):
-        #     search_type = browser.find_element_by_xpath('//select[@name="TYPE"]/option[@value="CNTR"]')
-        #     search_type.click()
-        if browser.find_element_by_xpath('//input[@name="CargoNO"]'):
-            search_input = browser.find_element_by_xpath('//input[@name="CargoNO"]')
-            search_input.click()
-            search_input.send_keys('LEAU4908755')
-        search_button = browser.find_element_by_xpath('//a[@href="javascript:submit(13)"]')
-        search_button.click()
+    def _parse_product_links(self, response):
 
-        page_content = browser.page_source
+        if response.xpath('//div[@class="product__image"]/a/@href').extract():
+            prods = response.xpath('//div[@class="product__image"]/a/@href').extract()
+            for prod in prods:
+                yield Request(url='https://www.electrictobacconist.com' + prod, callback=self.parse_product)
 
-        product = SiteProductItem()
-        product['Origin'] = page_content
+    def parse_product(self, response):
+        prod_item = SiteProductItem()
+        name = self._parse_name(response)
+        price = self._parse_price(response)
+        url = self._parse_url(response)
+        prod_item['Name'] = name
+        prod_item['Price'] = price
+        prod_item['Product_Url'] = url
+        return prod_item
 
-        return product
+    @staticmethod
+    def _parse_name(response):
+        if response.xpath('//span[@itemprop="name"]//text()').extract():
+            name = response.xpath('//span[@itemprop="name"]//text()').extract()[0]
+
+        return str(name) if name else " "
+
+    @staticmethod
+    def _parse_price(response):
+        if response.xpath('//span[@itemprop="price"]//text()').extract():
+            price = response.xpath('//span[@itemprop="price"]//text()').extract()
+
+        return str(price[0]) if price else " "
+
+    @staticmethod
+    def _parse_url(response):
+        url = response.url
+        return url
+
 
 
 

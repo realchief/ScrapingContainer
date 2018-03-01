627d6eda6065df0624ac2323df61ad06f4429653
softcomet <professinalcertifiedsenior.dev@gmail.com>
2018-03-01 06:31:48 +0100

    scraping data from http://www.hmm.co.kr

--- scraper/spiders/scrapingdata.py
+++ scraper/spiders/scrapingdata.py
@@ -4,9 +4,11 @@ from scrapy import Request
 import scrapy
 import re
 import json
+import csv
 import os
 import time
-import time
+import requests
+
 # from pyvirtualdisplay import Display
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
@@ -14,6 +16,22 @@ from selenium.webdriver.common.keys import Keys
 # CWD = os.path.dirname(os.path.abspath(__file__))
 # driver_path = os.path.join(CWD, 'bin', 'chromedriver')
 # driver_log_path = os.path.join(CWD, 'bin', 'driver.log')
+
+# keys = ['LEAU4908550', 'LEAU4908565', 'LEAU4908605', 'LEAU4908610', 'LEAU4908734', 'LEAU4908755']
+keys = ['SESU2195470', 'SESU2195253', 'SESU2195232', 'SESU2195227']
+# keys = []
+# try:
+#     with open(os.path.abspath('inputdata.csv'), 'r') as f:
+#         reader = csv.reader(f)
+#         for row in reader:
+#             keys.append(row[0])
+# except Exception as e:
+#     print('parse_csv Function => Got Error: {}'.format(e))
+#
+#     with open('/home/ubuntu/Marin-Guru/container_scraping/ScrapingContainer-MySQL/inputdata/inputdata.csv', 'r') as f:
+#         reader = csv.reader(f)
+#         for row in reader:
+#             keys.append(row[0])
 
 
 class SiteProductItem(scrapy.Item):
@@ -29,25 +47,54 @@ class SiteProductItem(scrapy.Item):
 class NewEvents (scrapy.Spider):
 
     name = "scrapingdata"
-    allowed_domains = ['www.shipmentlink.com']
-    start_urls = ['https://www.shipmentlink.com/servlet/TDB1_CargoTracking.do']
-    form_data = {'CNTR': 'LEAU4908755', 'TYPE': 'CNTR'}
-    headers = {'Referer': 'https://www.shipmentlink.com/servlet/TDB1_CargoTracking.do',
-               'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                             'Chrome/64.0.3282.186 Safari/537.36',
-               'X-Requested-With': 'XMLHttpRequest',
-               'Content-Type': 'application/x-www-form-urlencoded'
-               }
+    allowed_domains = ['www.shipmentlink.com', 'www.hmm.co.kr']
+    # start_urls = ['https://www.shipmentlink.com/servlet/TDB1_CargoTracking.do',
+    #               'http://www.hmm.co.kr/cms/business/ebiz/trackTrace/trackTrace'
+    #               '/index.jsp?type=2&number=SESU2195470&is_quick=Y&quick_params=',
+    #               ]
+    start_urls = ['http://www.hmm.co.kr/ebiz/track_trace/trackCTPv8.jsp?'
+                  'blFields=undefined&cnFields=undefined&numbers=&numbers='
+                  '&numbers=&numbers=&numbers=&numbers=&numbers=&numbers='
+                  '&numbers=&numbers=SESU2195470&numbers=&numbers=&numbers=&numbers='
+                  '&numbers=&numbers=&numbers=&numbers=&numbers=&numbers=&numbers=&numbers=&numbers=&numbers=']
+
+    # start_urls = ['https://www.shipmentlink.com/servlet/TDB1_CargoTracking.do']
+    # headers = {
+    #            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) '
+    #                          'Chrome/64.0.3282.186 Safari/537.36',
+    #            'Content-Type': 'application/x-www-form-urlencoded'
+    #            }
 
     def start_requests(self):
-        start_urls = self.start_urls
-        yield Request(url=start_urls[0],
-                      callback=self.parse_product,
-                      headers=self.headers,
-                      method='POST',
-                      body=urlencode(self.form_data),
-                      dont_filter=True
-                      )
+        for start_url in self.start_urls:
+            for key in keys:
+                if 'www.shipmentlink.com' in start_url:
+                    form_data = {'CNTR': key, 'TYPE': 'CNTR', 'blFields': '1', 'cnFields': '1', 'is-quick': 'Y'}
+                    yield Request(url=start_url,
+                                  headers={
+                                            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                                            'Chrome/64.0.3282.186 Safari/537.36',
+                                            'Content-Type': 'application/x-www-form-urlencoded'
+                                           },
+                                  callback=self.parse_product,
+                                  method='POST',
+                                  body=urlencode(form_data),
+                                  dont_filter=True)
+
+                if 'www.hmm.co.kr' in start_url:
+                    form_data = {'number': 'SESU2195470', 'numbers': 'SESU2195470'}
+                    yield Request(url=start_url,
+                                  headers={
+                                      'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko)'
+                                                    ' Chrome/64.0.3282.186 Safari/537.36',
+                                      'Content-Type': 'application/x-www-form-urlencoded',
+                                      'Referer': 'http://www.hmm.co.kr/ebiz/track_trace/main_new.jsp?type=2&number=SESU2195470'
+                                                 '&is_quick=Y&quick_params='
+                                  },
+                                  method='POST',
+                                  callback=self.parse_product,
+                                  body=urlencode(form_data),
+                                  dont_filter=True)
 
     def parse_product(self, response):
 
@@ -65,38 +112,60 @@ class NewEvents (scrapy.Spider):
 
     @staticmethod
     def _parse_ContainerNumber(response):
-        ContainerNumber = response.xpath('//table[@width="95%"][3]/tr[3]/td[1]//text()').extract()
-        return str(ContainerNumber[0]) if ContainerNumber else ' '
+        try:
+            ContainerNumber = response.xpath('//table[@width="95%"][3]/tr[3]/td[1]//text()').extract()
+            return str(ContainerNumber[0]) if ContainerNumber else ' '
+        except Exception as e:
+            print('Error')
 
     @staticmethod
     def _parse_ContainerSizeType(response):
-        ContainerSizeType = response.xpath('//table[@width="95%"][3]/tr[3]/td[2]//text()').extract()
-        return str(ContainerSizeType[0]).strip() if ContainerSizeType else ' '
+        try:
+            ContainerSizeType = response.xpath('//table[@width="95%"][3]/tr[3]/td[2]//text()').extract()
+            return str(ContainerSizeType[0]).strip() if ContainerSizeType else ' '
+        except Exception as e:
+            print('Error')
 
     @staticmethod
     def _parse_Date(response):
-        Date = response.xpath('//table[@width="95%"][3]/tr[3]/td[3]//text()').extract()
-        return str(Date[0]).strip() if Date else ' '
+        try:
+            Date = response.xpath('//table[@width="95%"][3]/tr[3]/td[3]//text()').extract()
+            return str(Date[0]).strip() if Date else ' '
+        except Exception as e:
+            print('Error')
 
     @staticmethod
     def _parse_ContainerMoves(response):
-        ContainerMoves = response.xpath('//table[@width="95%"][3]/tr[3]/td[4]//text()').extract()
-        return str(ContainerMoves[0]).strip() if ContainerMoves else ' '
+        try:
+            ContainerMoves = response.xpath('//table[@width="95%"][3]/tr[3]/td[4]//text()').extract()
+            return str(ContainerMoves[0]).strip() if ContainerMoves else ' '
+        except Exception as e:
+            print('Error')
 
     @staticmethod
     def _parse_Location(response):
-        Location = response.xpath('//table[@width="95%"][3]/tr[3]/td[5]//text()').extract()
-        return str(Location[0]).strip() if Location else ' '
+        try:
+            Location = response.xpath('//table[@width="95%"][3]/tr[3]/td[5]//text()').extract()
+            return str(Location[0]).strip() if Location else ' '
+        except Exception as e:
+            print('Error')
 
     @staticmethod
     def _parse_VesselVoyage(response):
-        VesselVoyage = response.xpath('//table[@width="95%"][3]/tr[3]/td[6]//text()').extract()
-        return str(VesselVoyage[0]).strip() if VesselVoyage else ' '
+        try:
+            VesselVoyage = response.xpath('//table[@width="95%"][3]/tr[3]/td[6]//text()').extract()
+            return str(VesselVoyage[0]).strip() if VesselVoyage else ' '
+        except Exception as e:
+            print('Error')
 
     @staticmethod
     def _parse_VGM(response):
-        VGM = response.xpath('//table[@width="95%"][3]/tr[3]/td[8]//text()').extract()
-        return str(VGM[0]).strip() if VGM else ' '
+        try:
+            VGM = response.xpath('//table[@width="95%"][3]/tr[3]/td[8]//text()').extract()
+            return str(VGM[0]).strip() if VGM else ' '
+        except Exception as e:
+            print('Error')
+
 
 
 

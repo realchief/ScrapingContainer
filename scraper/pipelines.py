2ea6ae435ac2d69bf4982863ad905d29235a5d10
realchief <stryokka@gmail.com>
2018-03-30 02:28:01 +0200

    remove unnecssary codes

--- scraper/pipelines.py
+++ scraper/pipelines.py
@@ -5,9 +5,6 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
-import MySQLdb
-import traceback
-
 
 class ScraperPipeline(object):
     def process_item(self, item, spider):
